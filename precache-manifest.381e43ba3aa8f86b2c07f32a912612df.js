a3a5b8b805677e0bb6b7dad8f1fa8affa78381f7
dragonfist453 <thermosphere453@gmail.com>
2020-08-06 01:27:44 +0530

    Updates

---
+++ precache-manifest.381e43ba3aa8f86b2c07f32a912612df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c7f6720ca1b960256c7bb00b3f2ae44",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a203632d4a609048ccb2",
+    "url": "/portfolio/static/css/2.ed439b8d.chunk.css"
+  },
+  {
+    "revision": "a6ed0d7f4522e268764d",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a203632d4a609048ccb2",
+    "url": "/portfolio/static/js/2.94a1cd88.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.94a1cd88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6ed0d7f4522e268764d",
+    "url": "/portfolio/static/js/main.7066d568.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
